4909bdbe0378d560416b57e43cf436828e865e3f
evanicela <niyonizigiye.celestin@gmail.com>
2023-03-27 00:40:04 +0300

    first commit

---
+++ src/main/kotlin/assignment.kt
@@ -0,0 +1,48 @@
+//1.
+
+fun main() {
+    val list1: IntArray = intArrayOf(1,2, 3, 4, 5, 6,7,8)
+    val list2: IntArray = intArrayOf(1,2, 3, 4, 5, 6,7,9)
+
+    val sum = list1.sum() + list2.sum()
+    println("The sum is $sum")
+
+
+    // 2.list of integers and returns the sum
+
+    val list3: IntArray = intArrayOf(1,2, 3, 4, 5, 6,7,8)
+    val mysum = list3.sum()
+    println("The sum is $mysum")
+
+
+
+//5 removing duplicate
+
+
+
+    val list= listOf("level", "sept", "level")
+
+    val removing = list.toSet().toList();
+
+    println(removing)
+
+
+    // 3. Palindrome
+
+    print("Enter a string : ")
+    val str = readln()
+
+    if (str == str.reversed()) {
+        print("$str is palindrome.")
+    } else {
+        print("$str is not palindrome.")
+    }
+
+
+
+}
+
+
+
+
+
